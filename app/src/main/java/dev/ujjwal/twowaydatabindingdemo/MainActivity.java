ab23f3e72c21ce73e7df122b9662c6a19b5bc5c7
ujjwal <ujjwalmaity98@gmail.com>
2019-10-31 16:18:57 +0530

    Do simple data binding

--- app/src/main/java/dev/ujjwal/twowaydatabindingdemo/MainActivity.java
+++ app/src/main/java/dev/ujjwal/twowaydatabindingdemo/MainActivity.java
@@ -1,14 +1,40 @@
 package dev.ujjwal.twowaydatabindingdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import dev.ujjwal.twowaydatabindingdemo.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        SampleText sampleText = new SampleText("UJJWAL MAITY");
+        activityMainBinding.setSample(sampleText);
+    }
+}
+
+class SampleText {
+
+    private String text;
+
+    SampleText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
